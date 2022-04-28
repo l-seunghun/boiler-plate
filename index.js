c5e6e099179dfda64f8bfbf8c865144877e6f790
l-seunghun <93120481+l-seunghun@users.noreply.github.com>
2022-04-28 16:49:35 +0900

    boiler-plate init

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const {Users} = require('./models/Users');
+const bodyParser = require('body-parser');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://webzzang79:qwe123@cluster0.rvmye.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   // useNewUrlParser :true
@@ -10,11 +17,26 @@ mongoose.connect('mongodb+srv://webzzang79:qwe123@cluster0.rvmye.mongodb.net/myF
   // , useCreateIndex:true
   // , useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 db에 insert
+
+  const users = new Users(req.body);
+  users.save((err, userInfo) => {
+    if(err)return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   name:{
@@ -30,8 +30,8 @@ const userSchema = mongoose.Schema({
     type:Number
   }
 
-})
+});
 
-const user = mongoose.model('user', userSchema)
+const Users = mongoose.model('Users', userSchema);
 
-module.exports = {user}
+module.exports = {Users};
